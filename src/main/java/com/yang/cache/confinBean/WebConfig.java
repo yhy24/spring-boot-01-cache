48bd6729c30a608a323d3e4faa27405a415581d8
yanghy <15737313694@163.com>
2019-05-16 17:47:21 +0800

    添加过滤器，暂不处理业务

--- src/main/java/com/yang/cache/confinBean/WebConfig.java
+++ src/main/java/com/yang/cache/confinBean/WebConfig.java
@@ -1,6 +1,8 @@
 package com.yang.cache.confinBean;
 
+import com.yang.cache.intercepter.DataFilter;
 import com.yang.cache.intercepter.PrivateIntercepter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "com.yang.cache.intercepter.**",
         "com.yang.cache.controller.**"})
 public class WebConfig implements WebMvcConfigurer {
+//    拦截器的使用
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(privateIntercepter()).addPathPatterns("/**");
@@ -26,5 +29,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public PrivateIntercepter privateIntercepter() {
         return new PrivateIntercepter();
+    }
+/*注册过滤器的使用*/
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterBean = new FilterRegistrationBean();
+        filterBean.setFilter(new DataFilter());
+        filterBean.addUrlPatterns("/*");
+        filterBean.setName("dataFilter");
+        filterBean.setOrder(0);
+        return filterBean;
     }
 }
---
+++ src/main/java/com/yang/cache/intercepter/DataFilter.java
@@ -0,0 +1,39 @@
+package com.yang.cache.intercepter;
+
+import org.springframework.web.util.UrlPathHelper;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @Author: yhy
+ * @Date: 2019/5/16 17:23
+ * @Version 1.0
+ */
+@WebFilter(urlPatterns = "/*", filterName = "dataFilter")
+public class DataFilter implements Filter {
+    private UrlPathHelper helper = new UrlPathHelper();
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("******init()******");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("******doFilter()******");
+        String url = helper.getLookupPathForRequest((HttpServletRequest) request);
+        if (url.startsWith("/user")) {
+        //TODO 处理业务逻辑的使用
+            chain.doFilter(request, response);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("******destroy()******");
+    }
+}
